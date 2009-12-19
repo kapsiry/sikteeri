f87210b1dbaa6bfdc1163b2bfec9a9086f02202d
Joonas Kortesalmi <joneskoo@grant.(none)>
2009-12-19 15:41:31 +0200

    Finnish translations, added joneskoo git repository url

--- pull.sh
+++ pull.sh
@@ -8,8 +8,10 @@ remote () {
 remote gua https://gua.kapsi.fi/git/django-sikteeri.git
 remote hukka http://koti.kapsi.fi/hukka/django-sikteeri.git
 remote jkj http://koti.kapsi.fi/oh8glv/django-sikteeri.git
+remote joneskoo http://joneskoo.kapsi.fi/django-sikteeri.git
 
 git pull gua master
 git pull hukka master
 git pull jkj master
+git pull joneskoo master
 
