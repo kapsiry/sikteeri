2ac66347dece602317b2709b240125996002f10d
Ilkka Mattila <ilkka@lakka.kapsi.fi>
2009-12-19 20:55:09 +0200

    Membership refactor: Contact object

--- membership/admin.py
+++ membership/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from models import *
 
 admin.site.register(Membership)
+admin.site.register(Contact)
 admin.site.register(Alias)
 admin.site.register(Fee)
 admin.site.register(BillingCycle)
