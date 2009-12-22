cd1df350611f7092b6a5f7266e83dd376687f2ed
Juho Juopperi <jkj@snekokat.lan>
2009-12-22 22:52:01 +0200

    hukka first to have less conflicts

--- pull.sh
+++ pull.sh
@@ -5,14 +5,14 @@ remote () {
 	git remote show $1 >/dev/null 2>&1 && return # skip existing
 	git remote add -t master -m master $@
 }
-remote gua https://gua.kapsi.fi/git/django-sikteeri.git
 remote hukka http://koti.kapsi.fi/hukka/django-sikteeri.git
+remote gua https://gua.kapsi.fi/git/django-sikteeri.git
 remote jkj http://koti.kapsi.fi/oh8glv/django-sikteeri.git
 remote joneskoo http://joneskoo.kapsi.fi/django-sikteeri.git
 remote ilkka http://koti.kapsi.fi/ilkka/django-sikteeri.git
 
-git pull gua master
 git pull hukka master
+git pull gua master
 git pull jkj master
 git pull joneskoo master
 git pull ilkka master
