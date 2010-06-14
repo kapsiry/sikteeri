fa3383c2f92378bdda3200b97d2079f8a17ef51a
Joonas Kortesalmi <joneskoo@kapsi.fi>
2010-06-15 00:46:27 +0300

    Rewrote install-virtualenv.sh to be safer

--- install-virtualenv.sh
+++ install-virtualenv.sh
@@ -1,7 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
-hg clone http://bitbucket.org/ianb/virtualenv/ tmp-virtualenv
-python tmp-virtualenv/virtualenv.py env
-rm -rf tmp-virtualenv
-source env/bin/activate
-./env/bin/easy_install django
+# install-virtualenv.sh
+#
+# Install virtualenv environment for sikteeri development
+# to "env/" if it doesn't exist already
+
+VIRTUALENV=virtualenv
+ENVDIR=env
+
+if [[ -n $1 ]]; then
+    VIRTUALENV=$1
+fi
+
+if [[ ! -x `which $VIRTUALENV` ]]; then
+    echo "Please install virtualenv first."
+    echo
+    echo "  sudo apt-get install python-virtualenv (Debian/Ubuntu)"
+    echo "  sudo easy_install virtualenv (OS X, others?)"
+    echo
+    echo "If you have installed it already, try running"
+    echo
+    echo "$(basename $0) <path-to-virtualenv>"
+    exit 1
+fi
+
+function fatal () {
+    echo $*
+    exit 1
+}
+
+test -a $ENVDIR && fatal "$ENVDIR already exists"
+
+$VIRTUALENV $ENVDIR || fatal "Failed to create virtualenv $ENVDIR"
+
+source $ENVDIR/bin/activate
+
+if [[ ! -a $ENVDIR/bin/pip ]]; then
+    $ENVDIR/bin/easy_install pip || fatal "Could not install pip in virtualenv"
+fi
+
+for pkg in django simplejson; do
+    $ENVDIR/bin/pip install $pkg || fatal "Could not install $pkg in $ENVDIR"
+done
+
+echo "Virtualenv environment $ENVDIR done"
+echo "To later activate the environment, type"
+echo "  . env/bin/activate"
+exit 0
