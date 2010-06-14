bdaa81c2418fff84486b38b7add3b4e50b740a27
Joonas Kortesalmi <joneskoo@kapsi.fi>
2010-06-15 01:01:32 +0300

    Adding CSRF tokens to all form posts and enabling middleware

--- sikteeri/settings.py
+++ sikteeri/settings.py
@@ -55,6 +55,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
