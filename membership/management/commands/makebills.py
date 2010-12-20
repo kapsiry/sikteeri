09fa9e73af1d6a62b919ac9713a6d01039d62c53
Atte Hinkka <atte.hinkka@iki.fi>
2010-12-20 17:37:22 +0200

    Added TODO note and fixed imports.

--- membership/management/commands/makebills.py
+++ membership/management/commands/makebills.py
@@ -1,7 +1,15 @@
 from django.core.management.base import NoArgsCommand
 
+import datetime
+
 from membership.models import *
 from membership.utils import *
+
+# TODO: this command should also generate billing cycles
+# billing_cycle = BillingCycle(membership=self)
+# billing_cycle.save() # Creating an instance does not touch db and we need and id for the Bill
+# bill = Bill(billingcycle=billing_cycle)
+# bill.save()
 
 class Command(NoArgsCommand):
     help = 'Find expiring billing cycles, send bills, send reminders'
@@ -12,7 +20,7 @@ class Command(NoArgsCommand):
                 billingcycle = member.billingcycle_set.order_by('-end')[0]
             except:
                 continue
-            if billingcycle.end < datetime.now() + timedelta(days=28):
+            if billingcycle.end < datetime.now() + datetime.timedelta(days=28):
                 new_cycle(member)
                 print "New billing cycle"
                 continue
@@ -20,8 +28,8 @@ class Command(NoArgsCommand):
                 continue
             
             # FIXME: should reflect last bill due date, not billing cycle start
-            if billingcycle.start < datetime.now() + timedelta(days=7)
-                and len(billingcycle.bill_set.all()) == 1:
+            if billingcycle.start < datetime.now() + datetime.timedelta(days=7) \
+                   and len(billingcycle.bill_set.all()) == 1:
                 sendreminder(member)
                 print "Reminder"
             elif billingcycle.start < datetime.now():
