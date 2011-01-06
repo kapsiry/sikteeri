b5241c7ab965d054856907dd5d2db0175b51c08e
Joonas Kuorilehto <joneskoo@kapsi.fi>
2011-01-06 17:41:08 +0200

    OP format CSV parser implemented, moved is_paid
    Implemented a CSV parser for the Osuuspankki CSV
    format. Header names are translated to short
    values. All decoded data is Python unicode
    strings. Also moved is_paid marking so that it is
    only checked when a payment concerning the bill
    is found.

--- membership/management/commands/csvbills.py
+++ membership/management/commands/csvbills.py
@@ -1,40 +1,138 @@
+# encoding: UTF-8
+
 from __future__ import with_statement
 
+from django.db.models import Q, Sum
+from django.core.management.base import LabelCommand
+from django.core.exceptions import ObjectDoesNotExist
+
+import codecs
+import csv
 import os
+
 from datetime import datetime
-from decimal import Decimal, getcontext
+from decimal import Decimal
+
 import logging
 logger = logging.getLogger("sikteeri.membership.management.commands.csvbills")
-import csv
 
-from django.db.models import Q, F, Sum
-from django.core.management.base import LabelCommand
+from membership.models import Bill, BillingCycle, Payment
 
-from membership.models import *
-from membership.utils import *
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+
+    <http://docs.python.org/library/csv.html#examples>
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+
+    <http://docs.python.org/library/csv.html#examples>
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+class UnicodeDictReader(UnicodeReader):
+    """A CSV reader which stores the headers from the first line
+    """
+    def __init__(self, *args, **kw):
+        UnicodeReader.__init__(self, *args, **kw)
+        # Read headers from first line
+        self.headers = UnicodeReader.next(self)
+
+    def next(self):
+        row = UnicodeReader.next(self)
+        return dict(zip(self.headers, row))
+
+class RequiredFieldNotFoundException(Exception): pass
+
+class OpDictReader(UnicodeDictReader):
+    '''Reader for Osuuspankki CSV file format
+
+    The module converts Osuuspankki CSV format data into a more usable form.'''
+
+    # If these fields are not found on the first line, an exception is raised
+    REQUIRED_COLUMNS = ['date', 'amount', 'transaction']
+
+    # Translation table from Osuuspankki CSV format to short names
+    OP_CSV_TRANSLATION = {u'Kirjauspäivä'       : 'date',
+                          u'Arvopäivä'          : 'value_date',
+                          u'Määrä EUROA'        : 'amount',
+                          u'Tapahtumalajikoodi' : 'event_type_code',
+                          u'Selitys'            : 'event_type_description',
+                          u'Saaja/Maksaja'      : 'fromto',
+                          u'Saajan tilinumero'  : 'account',
+                          u'Viite'              : 'reference',
+                          u'Viesti'             : 'message',
+                          u'Arkistotunnus'      : 'transaction'}
+
+    def __init__(self, f, delimiter=';', encoding="iso8859-1", *args, **kw):
+        UnicodeDictReader.__init__(self, f, delimiter=delimiter,
+            encoding=encoding, *args, **kw)
+        # Translate headers
+        h = self.headers
+        for i in xrange(0, len(h)):
+            self.headers[i] = self.OP_CSV_TRANSLATION.get(h[i], h[i])
+        # Check that all required columns exist in the header
+        for name in self.REQUIRED_COLUMNS:
+            if name not in self.headers:
+                raise RequiredFieldNotFoundException("CSV format is invalid")
+
+    def next(self):
+        row = UnicodeDictReader.next(self)
+        if len(row) == 0:
+            return None
+        row['amount'] = Decimal(row['amount'].replace(",", "."))
+        row['date'] = datetime.strptime(row['date'], "%d.%m.%Y")
+        if row.has_key('value_date'):
+            row['value_date'] = datetime.strptime(row['value_date'], "%d.%m.%Y")
+        return row
 
 def row_to_payment(row):
     # FIXME: should replace decodes with a decoding CSV 'dialect'
-    transaction_id = row['arkistointitunnus'].decode("ISO8859-1")
     try:
-        p = Payment.objects.get(transaction_id__exact=transaction_id)
+        p = Payment.objects.get(transaction_id__exact=row['transaction'])
         return p
-    except Payment.DoesNotExist, dne:
-        p = Payment(payment_day=datetime.strptime(row['pvm'], "%Y%m%d"),
-                    amount=Decimal(row['summa'].replace(",", ".")),
-                    type=row['tapahtuma'].decode("ISO8859-1"),
-                    payer_name=row['nimi'].decode("ISO8859-1"),
-                    reference_number=row['viite'].decode("ISO8859-1"),
-                    message=row['viesti'].decode("ISO8859-1"),
-                    transaction_id=transaction_id)
+    except Payment.DoesNotExist:
+        p = Payment(payment_day=row['date'],
+                    amount=row['amount'],
+                    type=row['event_type_description'],
+                    payer_name=row['fromto'],
+                    reference_number=row['reference'],
+                    message=row['message'],
+                    transaction_id=row['transaction'])
     return p
 
-def process_csv(file):
-    with open(file, 'r') as f:
-        reader = csv.DictReader(f, delimiter=',')
+def process_csv(filename):
+    """Actual CSV file processing logic
+    """
+    with open(filename, 'r') as f:
+        reader = OpDictReader(f)
         for row in reader:
-            amount = Decimal(row['summa'].replace(",", "."))
-            if amount < 0: # Transaction is paid by us, ignored
+            if row == None:
+                continue
+            if row['amount'] < 0: # Transaction is paid by us, ignored
                 continue
             payment = row_to_payment(row)
 
@@ -44,22 +142,22 @@ def process_csv(file):
                 continue
 
             try:
-                q = Q(reference_number=payment.reference_number)
-                bill = Bill.objects.filter(q).order_by("-due_date")[0]
+                ref = Q(billingcycle__reference_number=payment.reference_number)
+                bill = Bill.objects.filter(ref).latest("due_date")
                 bill.save()
-            except IndexError:
+            except ObjectDoesNotExist:
                 continue # Failed to find bill for this reference number
             payment.bill = bill
             payment.save()
             logger.info("Payment %s attached to bill %s." % (
                 repr(payment), repr(bill)))
+            cycle = bill.billingcycle
+            cycle.annotate(payments_sum=Sum('bill__payment__amount'))
+            if cycle.payments_sum >= cycle.sum:
+                cycle.is_paid = True
+                cycle.save()
+                logger.info("Bill %s marked as paid." % (repr(bill)))
 
-    # For each unpaid billing cycle, check if paid now
-    # FIXME: might not fill specifications. Does it work? :)
-    # Needs work and testing.
-    cycles = BillingCycle.objects.all().annotate(payments_sum=Sum('bill__payment__amount'))
-    paid_cycles = cycles.filter(payments_sum__gte=F('sum'))
-    paid_cycles.update(is_paid=True)
 
 class Command(LabelCommand):
     help = 'Find expiring billing cycles, send bills, send reminders'
