9596615a7ca3eb4e5154e24aa2e49dea976ccd50
Henri Strand <str4nd@github.mail.kapsi.fi>
2011-03-04 15:52:22 +0200

    Fixed indents.

--- membership/management/commands/public_memberlist.py
+++ membership/management/commands/public_memberlist.py
@@ -8,14 +8,14 @@ from django.conf import settings
 from membership.models import *
 
 class Command(NoArgsCommand):
-	def handle_noargs(self, **options):
-		mship = Membership.objects.filter(status__exact='A')
-		count = mship.count()
-		mship = mship.filter(public_memberlist__exact="True")
-		mship = mship.order_by("person__last_name", "person__first_name")
-		person_q = Q(person_set__in=mship)
-		person_contacts = Contact.objects.filter(person_q)
-		return render_to_string('membership/public_memberlist.xml', {
-		                          'count': count,
+    def handle_noargs(self, **options):
+        mship = Membership.objects.filter(status__exact='A')
+        count = mship.count()
+        mship = mship.filter(public_memberlist__exact="True")
+        mship = mship.order_by("person__last_name", "person__first_name")
+        person_q = Q(person_set__in=mship)
+        person_contacts = Contact.objects.filter(person_q)
+        return render_to_string('membership/public_memberlist.xml', {
+                                  'count': count,
 		                          'member_list': person_contacts
-		                       } ).encode('utf-8')
+                               } ).encode('utf-8')
