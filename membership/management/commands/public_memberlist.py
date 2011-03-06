f9911891d8a713c7a2ac50993a76642c9d5dadbd
Ilkka Mattila <git@ilkka.kapsi.fi>
2011-03-06 21:48:58 +0200

    Exclude system member from public_memberlist

--- membership/management/commands/public_memberlist.py
+++ membership/management/commands/public_memberlist.py
@@ -9,7 +9,7 @@ from membership.models import *
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        mship = Membership.objects.filter(status__exact='A')
+        mship = Membership.objects.filter(status__exact='A', id__gt=0)
         count = mship.count()
         mship = mship.filter(public_memberlist__exact="True").order_by('person__last_name', 'person__first_name')
         return render_to_string('membership/public_memberlist.xml', {
