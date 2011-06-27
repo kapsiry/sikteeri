43cd043ba3185938d881ef688dc41bcd35a9e881
Ilkka Mattila <git@ilkka.kapsi.fi>
2011-06-28 00:49:51 +0300

    Send reminders to additional addresses

--- membership/email_utils.py
+++ membership/email_utils.py
@@ -14,24 +14,42 @@ from django.dispatch import Signal
 send_as_email = Signal(providing_args=["instance"])
 send_preapprove_email = Signal(providing_args=["instance", "user"])
 
+# Address helper
+def unix_email(membership):
+    if settings.UNIX_EMAIL_DOMAIN:
+        from services.models import valid_aliases, Alias
+        try:
+            alias = valid_aliases(membership).filter(account=True).latest('created')
+            return "%s <%s@%s>" % (membership.name(), alias, settings.UNIX_EMAIL_DOMAIN)
+        except Alias.DoesNotExist:
+            pass
+    return None
+
 # Signal handlers
 def bill_sender(sender, instance=None, **kwargs):
     membership = instance.billingcycle.membership
+    to = [membership.billing_email()]
+    if instance.is_reminder():
+        if membership.billing_contact:
+            to.append(membership.email_to())
+        local_email = unix_email(membership)
+        if local_email:
+            to.append(local_email)
     if settings.BILLING_CC_EMAIL != None:
         email = EmailMessage(instance.bill_subject(),
                              instance.render_as_text(),
                              settings.BILLING_FROM_EMAIL,
-                             [membership.billing_email()],
+                             to,
                              [settings.BILLING_CC_EMAIL],
                              headers={'CC': settings.BILLING_CC_EMAIL})
     else:
         email = EmailMessage(instance.bill_subject(),
                              instance.render_as_text(),
                              settings.BILLING_FROM_EMAIL,
-                             [membership.billing_email()])
+                             to)
     connection = mail.get_connection()
     connection.send_messages([email])
-    logger.info(u'A bill sent as email to %s: %s' % (membership.billing_email(),
+    logger.info(u'A bill sent as email to %s: %s' % (",".join(to),
                                                      unicode(instance)))
 
 def preapprove_email_sender(sender, instance=None, user=None, **kwargs):
@@ -51,6 +69,6 @@ def preapprove_email_sender(sender, instance=None, user=None, **kwargs):
                                   headers = {'Reply-To': instance.email_to()})
     connection = mail.get_connection()
     connection.send_messages([sysadmin_email])
-    logger.info(u'A preapprove email sent to %s <%s> by %s' % (unicode(instance),
+    logger.info(u'A preapprove email sent to %s (%s) by %s' % (unicode(instance),
                                                                instance.billing_email(),
                                                                user))
