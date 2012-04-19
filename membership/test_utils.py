2e3b67c4dfd9be50696ca8fecce4a317b6845894
Antti Jaakkola <annttu@kapsi.fi>
2012-04-19 21:28:32 +0300

    Improved testdata functions to be more random

--- membership/test_utils.py
+++ membership/test_utils.py
@@ -1,38 +1,40 @@
 # -*- coding: utf-8 -*-
-from random import choice
+from random import choice, random, randint
 import logging
 
 # Finnish population register center's most popular first names for year 2009
-first_names = [
-    u"Maria", u"Juhani", u"Aino", u"Veeti", u"Emilia", u"Johannes", u"Venla",
-    u"Eetu", u"Sofia", u"Mikael", u"Emma", u"Onni", u"Olivia", u"Matias",
-    u"Ella", u"Aleksi", u"Aino", u"Olavi", u"Sofia", u"Leevi", u"Amanda",
-    u"Onni", u"Aada", u"Elias", u"Matilda", u"Ilmari", u"Sara", u"Lauri",
-    u"Helmi", u"Oskari", u"Iida", u"Joona", u"Aurora", u"Elias", u"Anni",
-    u"Matias", u"Ilona", u"Oliver", u"Helmi", u"Leo", u"Iida", u"Eemeli",
-    u"Emilia", u"Niilo", u"Eveliina", u"Valtteri", u"Siiri", u"Rasmus", u"Katariina",
-    u"Aleksi", u"Veera", u"Oliver", u"Ella", u"Antero", u"Sanni", u"Miro",
-    u"Aada", u"Viljami", u"Vilma", u"Jimi", u"Kristiina", u"Kristian", u"Nea",
-    u"Aatu", u"Anni", u"Tapani", u"Milla", u"Daniel", u"Johanna", u"Samuel",
-    u"Pinja", u"Juho", u"Emma", u"Lauri", u"Lotta", u"Aapo", u"Sara",
-    u"Tapio", u"Olivia", u"Eemeli", u"Linnea", u"Veeti", u"Elli", u"Jesse",
-    u"Anna", u"Eetu", u"Emmi", u"Arttu", u"Elina", u"Emil", u"Ronja",
-    u"Lenni", u"Venla", u"Petteri", u"Elsa", u"Valtteri", u"Julia", u"Daniel",
-    u"Nella", u"Otto", u"Aleksandra", u"Eemil", u"Kerttu", u"Aaro", u"Helena",
-    u"Juho", u"Oona", u"Joel", u"Siiri", u"Leevi", u"Viivi", u"Niklas",
-    u"Karoliina", u"Joona", u"Julia", u"Ville", u"Inkeri", u"Julius", u"Pihla",
-    u"Roope", u"Alexandra", u"Elmeri", u"Peppi", u"Konsta", u"Alisa", u"Leo",
-    u"Nelli", u"Juuso", u"Susanna", u"Otto", u"Neea", u"Luka", u"Josefiina",
-    u"Aleksanteri", u"Jenna", u"Mikael", u"Kaarina", u"Akseli", u"Laura", u"Samuel",
-    u"Lotta", u"Sakari", u"Anna", u"Oskari", u"Alina", u"Anton", u"Milja",
-    u"Julius", u"Ellen", u"Veikko", u"Enni", u"Luukas", u"Veera", u"Toivo",
-    u"Alisa", u"Jere", u"Sanni", u"Eino", u"Ilona", u"Niko", u"Kerttu",
-    u"Niilo", u"Inka", u"Eelis", u"Elsa", u"Jaakko", u"Amanda", u"Eeli",
-    u"Elli", u"Rasmus", u"Minea", u"Anton", u"Vilma", u"Antti", u"Matilda",
-    u"Eino", u"Vilhelmiina", u"Väinö", u"Iina", u"Emil", u"Nea", u"Henrik",
-    u"Eevi", u"Kasper", u"Anneli", u"Matti", u"Ellen", u"Tuomas", u"Maija",
-    u"Aatu", u"Saana", u"Eemil", u"Tuulia", u"Kalevi", u"Minttu", u"Akseli",
-    u"Anniina", u"Joonatan", u"Lilja", u"Viljami"]
+male_first_names = [
+    u'Juhani', u'Veeti', u'Johannes', u'Eetu', u'Mikael', u'Onni', u'Matias',
+    u'Aleksi', u'Olavi', u'Leevi', u'Onni', u'Elias', u'Ilmari', u'Lauri',
+    u'Oskari', u'Joona', u'Elias', u'Matias', u'Oliver', u'Leo', u'Eemeli',
+    u'Niilo', u'Valtteri', u'Rasmus', u'Aleksi', u'Oliver', u'Antero', u'Miro',
+    u'Viljami', u'Jimi', u'Kristian', u'Aatu', u'Tapani', u'Daniel', u'Samuel',
+    u'Juho', u'Lauri', u'Aapo', u'Tapio', u'Eemeli', u'Veeti', u'Jesse', u'Eetu',
+    u'Arttu', u'Emil', u'Lenni', u'Petteri', u'Valtteri', u'Daniel', u'Otto',
+    u'Eemil', u'Aaro', u'Juho', u'Joel', u'Leevi', u'Niklas', u'Joona', u'Ville',
+    u'Julius', u'Roope', u'Elmeri', u'Konsta', u'Leo', u'Juuso', u'Otto', u'Luka',
+    u'Aleksanteri', u'Mikael', u'Akseli', u'Samuel', u'Sakari', u'Oskari', u'Anton',
+    u'Julius', u'Veikko', u'Luukas', u'Toivo', u'Jere', u'Eino', u'Niko', u'Niilo',
+    u'Eelis', u'Jaakko', u'Eeli', u'Rasmus', u'Anton', u'Antti', u'Eino', u'Väinö',
+    u'Emil', u'Henrik', u'Kasper', u'Matti', u'Tuomas', u'Aatu', u'Eemil', u'Kalevi',
+    u'Akseli', u'Joonatan', u'Viljami'
+]
+
+flemale_first_names = [
+    u'Maria', u'Aino', u'Emilia', u'Venla', u'Sofia', u'Emma', u'Olivia', u'Ella',
+    u'Aino', u'Sofia', u'Amanda', u'Aada', u'Matilda', u'Sara', u'Helmi', u'Iida',
+    u'Aurora', u'Anni', u'Ilona', u'Helmi', u'Iida', u'Emilia', u'Eveliina', u'Siiri',
+    u'Katariina', u'Veera', u'Ella', u'Sanni', u'Aada', u'Vilma', u'Kristiina', u'Nea',
+    u'Anni', u'Milla', u'Johanna', u'Pinja', u'Emma', u'Lotta', u'Sara', u'Olivia',
+    u'Linnea', u'Elli', u'Anna', u'Emmi', u'Elina', u'Ronja', u'Venla', u'Elsa',
+    u'Julia', u'Nella', u'Aleksandra', u'Kerttu', u'Helena', u'Oona', u'Siiri',
+    u'Viivi', u'Karoliina', u'Julia', u'Inkeri', u'Pihla', u'Alexandra', u'Peppi',
+    u'Alisa', u'Nelli', u'Susanna', u'Neea', u'Josefiina', u'Jenna', u'Kaarina',
+    u'Laura', u'Lotta', u'Anna', u'Alina', u'Milja', u'Ellen', u'Enni', u'Veera',
+    u'Alisa', u'Sanni', u'Ilona', u'Kerttu', u'Inka', u'Elsa', u'Amanda', u'Elli',
+    u'Minea', u'Vilma', u'Matilda', u'Vilhelmiina', u'Iina', u'Nea', u'Eevi', u'Anneli',
+    u'Ellen', u'Maija', u'Saana', u'Tuulia', u'Minttu', u'Anniina', u'Lilja'
+    ]
 
 # Kapsi members public unique last name listing as of today.
 last_names = [
@@ -149,12 +151,98 @@ last_names = [
     u"Wikman", u"Yli-Hukka", u"Ylimäinen", u"Ylinen", u"Ylönen", u"Yrttikoski",
     u"Äijänen", u"Ärmänen"]
 
-def random_first_name():
-    return choice(first_names)
+municipalities = [
+    u"Akaa",u"Alajärvi",u"Alavus",u"Espoo",u"Forssa",u"Haapajärvi",u"Haapavesi",
+    u"Hamina",u"Hanko",u"Harjavalta",u"Heinola",u"Helsinki",u"Huittinen",u"Hyvinkää",
+    u"Hämeenlinna",u"Iisalmi",u"Ikaalinen",u"Imatra",u"Joensuu",u"Juankoski",u"Jyväskylä",
+    u"Jämsä",u"Järvenpää",u"Kaarina",u"Kajaani",u"Kalajoki",u"Kankaanpää",u"Kannus",
+    u"Karkkila",u"Kaskinen",u"Kauhajoki",u"Kauhava",u"Kauniainen",u"Kemi",u"Kemijärvi",
+    u"Kerava",u"Keuruu",u"Kitee",u"Kiuruvesi",u"Kokemäki",u"Kokkola",u"Kotka",u"Kouvola",
+    u"Kristiinankaupunki",u"Kuhmo",u"Kuopio",u"Kurikka",u"Kuusamo",u"Lahti",u"Laitila",
+    u"Lappeenranta",u"Lapua",u"Lieksa",u"Lohja",u"Loimaa",u"Loviisa",u"Maarianhamina",
+    u"Mikkeli",u"Mänttä-Vilppula",u"Naantali",u"Nilsiä",u"Nivala",u"Nokia",u"Nurmes",
+    u"Närpiö",u"Orimattila",u"Orivesi",u"Oulainen",u"Oulu",u"Outokumpu",u"Paimio",
+    u"Parainen",u"Parkano",u"Pieksämäki",u"Pietarsaari",u"Pori",u"Porvoo",u"Pudasjärvi",
+    u"Pyhäjärvi",u"Raahe",u"Raasepori",u"Raisio",u"Rauma",u"Riihimäki",u"Rovaniemi",
+    u"Saarijärvi",u"Salo",u"Sastamala",u"Savonlinna",u"Seinäjoki",u"Somero",u"Suonenjoki",
+    u"Tampere",u"Tornio",u"Turku",u"Ulvila",u"Uusikaarlepyy",u"Uusikaupunki",u"Vaasa",
+    u"Valkeakoski",u"Vantaa",u"Varkaus",u"Viitasaari",u"Virrat",u"Ylivieska",u"Ylöjärvi",
+    u"Ähtäri",u"Äänekoski"
+    ]
+
+email_provders = [
+    u"gmail.com", u"google.com", u"kapsi.fi", u"hotmail.com", u"company.com", u"nokia.fi",
+    u"provider.org", u"freeemail.com", u"mail.ru", u"example.com", u"iki.fi", u"yahoo.com",
+    u"invalid.ccom",u"aol.com",u"msn.com",u"comcast.net", u"hotmail.co.uk",u"sbcglobal.net",
+    u"yahoo.co.uk",u"yahoo.co.in",u"bellsouth.net", u"verizon.net",u"earthlink.net",u"cox.net",
+    u"rediffmail.com",u"yahoo.ca",u"btinternet.com",u"charter.net",u"ntlworld.com"
+    ]
+
+street_addresses = [
+    u"Aittakuja",u"Ala-Topparintie",u"Alatie",u"Alhontie",u"Emännänmutka",u"Emännäntie",
+    u"Hakalanrinne",u"Hakalantie",u"Hakatie",u"Hakolantie",u"Hakotie",u"Hannantie",
+    u"Harjutie",u"Hauskalantie",u"Hinkankuja",u"Hollihaantie",u"Hollipolku",u"Honkatie",
+    u"Huhtalantie",u"Ikolantie",u"Isonkylän",u"Isännäntie",u"Jalmarintie",u"Jukolantie",
+    u"Kaanaantie",u"Kaarnatie",u"Kaarnikka",u"Kaislaranta",u"Kanervatie",u"Kanervikkopolku",
+    u"Karoliinankuja",u"Katajamäentie",u"Katajatie",u"Kaukolanranta",u"Kauppakuja",
+    u"Kauppilantie",u"Kauppilanvälitie",u"Kekäletie",u"Keskustie",u"Ketotie",u"Ketunmutka",
+    u"Ketunpolku",u"Kiehispolku",u"Kinkorinne",u"Kinkovuorentie",u"Kirkkokankaantie",
+    u"Kirkkokuja",u"Kirkkotie",u"Kivimäentie",u"Koivikontie",u"Koivuahteentie",u"Koivumäentie",
+    u"Koivuniementie",u"Koivutie",u"Korpikoskentie",u"Korteelantie",u"Kotimäenpolku",
+    u"Kotimäentie",u"Koulutie",u"Kuivamäentie",u"Kumurinmaanpolku",u"Kunnastie",u"Kuulankatu",
+    u"Kuusimäentie",u"Kylätie",u"Kyrönmaantie",u"Kytönevantie",u"Kytötie",u"Kämppätie",u"Laaksotie",
+    u"Lakiankuja",u"Larvatie",u"Lauttamuksentie",u"Lehtokuja",u"Leppätie",u"Lietniementie",
+    u"Linjamanpolku",u"Louhikonmäentie",u"Lukuhaara",u"Lundintie",u"Lyytintie",u"Majatie",
+    u"Malkaniementie",u"Manninkuja",u"Marttilantie",u"Matinpalontie",u"Miilutie",u"Mäkilammintie",
+    u"Mäkitie",u"Mäntyniementie",u"Neulastie",u"Niementie",u"Nikkarintie",u"Notkotie",
+    u"Noukanniementie",u"Ojalankuja",u"Ojalantie",u"Paavalinranta",u"Pajutie",u"Palaspolku",
+    u"Papinpolku",u"Pappilanpolku",u"Peltolantie",u"Peltovainiontie",u"Perälänmäentie",
+    u"Peräläntie",u"Pihapolku",u"Pihlapolku",u"Pikkusaarenmäki",u"Pikkusaarentie",u"Pitkäjärventie",
+    u"Poikkisarkaantie",u"Poppelitie",u"Puimapolku",u"Rajala",u"Rannantie",u"Rantatie",
+    u"Ranteentie",u"Riihelänkuja",u"Riihikalliontie",u"Riihikuja",u"Riihitie",u"Rikuntie",
+    u"Rinnetie",u"Rinteentie",u"Ristimajanpolku",u"Roopelantie",u"Sanantie",u"Seinäjoentie",
+    u"Siikalankuja",u"Sompinkyläntie",u"Sompintie",u"Soratie",u"Suntiontie",u"Suvantotie",
+    u"Sysimetsäntie",u"Sysitie",u"Säynätsalontie",u"Tanhuantie",u"Teollisuustie",u"Teräsmäentie",
+    u"Tiltantie",u"Topparintie",u"Tuomipuro",u"Tuukikuja",u"Tyynitie",u"Uimarannantie",u"Uitonkuja",
+    u"Uitonsalmentie",u"Uljaantie",u"Vaasantie",u"Valkamantie",u"Varesvuontie",u"Varismäen",
+    u"Varismäentie",u"Varrasjärvenpolku",u"Vehkaojantie",u"Viitalantie",u"Vuorimäentie",
+    u"Välimaantie",u"Välinevantie",u"Ylistarontie",u"Yrittäjäntie",u"metsätie"
+]
+
+def random_sex():
+    if random() < 0.3:
+        return "f"
+    else:
+        return "m"
+
+def random_first_name(sex=None):
+    if sex is None:
+        sex = random_sex()
+    if sex is "f":
+        return choice(flemale_first_names)
+    else:
+        return choice(male_first_names)
 
 def random_last_name():
     return choice(last_names)
 
+def random_municipality():
+    return choice(municipalities)
+
+def random_street():
+    return choice(street_addresses)
+
+def random_email(firstname, lastname):
+    firstname = firstname.lower()
+    lastname = lastname.lower()
+    if random() < 0.7:
+        if random() > 0.55:
+            return ("%s.%s@%s" % (firstname, lastname, choice(email_provders)))
+        elif random() > 0.35:
+            return ("%s.%s.%s@%s" % (firstname, chr(randint(97,122)), lastname, choice(email_provders)))
+        else:
+            return ("%s_%s@%s" % (firstname, lastname, choice(email_provders)))
+    return ("%s%s@%s" % (firstname, randint(50,99), choice(email_provders)))
 
 class MockLoggingHandler(logging.Handler):
     """Mock logging handler to check for expected logs as per:
--- sikteeri/generate_test_data.py
+++ sikteeri/generate_test_data.py
@@ -12,7 +12,7 @@ from random import random, randint, choice
 from uuid import uuid4
 from decimal import Decimal
 import logging
-from membership.test_utils import random_first_name, random_last_name
+from membership.test_utils import random_first_name, random_last_name, random_municipality, random_email, random_street, random_sex
 logger = logging.getLogger("generate_test_data")
 
 from datetime import datetime
@@ -40,19 +40,21 @@ user = User.objects.get(id=1)
 
 @transaction.commit_manually
 def create_dummy_member(i):
-    fname = random_first_name()
+    sex = random_sex()
+    fname = random_first_name(sex)
+    lname = random_last_name()
     d = {
-        'street_address' : 'Testikatu %d'%i,
-        'postal_code' : '%d' % (i+1000),
-        'post_office' : 'Paska kaupunni',
+        'street_address' : '%s %d'% (random_street(), i),
+        'postal_code' : '%d' % randint(i+1000, 9999),
+        'post_office' : '%s' % random_municipality(),
         'country' : 'Finland',
         'phone' : "%09d" % (40123000 + i),
         'sms' : "%09d" % (40123000 + i),
-        'email' : 'user%d@example.com' % i,
+        'email' : random_email(fname, lname),
         'homepage' : 'http://www.example.com/%d'%i,
         'first_name' : fname,
-        'given_names' : '%s %s' % (fname, "Kapsi"),
-        'last_name' : random_last_name(),
+        'given_names' : '%s %s' % (fname, random_first_name(sex)),
+        'last_name' : lname,
     }
     person = Contact(**d)
     person.save()
@@ -60,8 +62,9 @@ def create_dummy_member(i):
     membership = Membership(type='P', status='N',
                             person=person,
                             nationality='Finnish',
-                            municipality='Paska kaupunni',
-                            extra_info='Hintsunlaisesti semmoisia tietoja.')
+                            municipality=random_municipality(),
+                            public_memberlist=random() > 0.7,
+                            extra_info='Tietoja tässä.')
 
     print unicode(person)
     membership.save()
@@ -101,7 +104,9 @@ def create_dummy_member(i):
     logger.info("New application %s from %s:." % (str(person), '::1'))
     return membership
 
+@transaction.commit_manually
 def create_payment(membership):
+    transaction.commit()
     if random() < 0.7:
         amount = "35.0"
         if random() < 0.2:
@@ -111,12 +116,13 @@ def create_payment(membership):
         if random() < 0.2:
             ref = str(randint(1000, 1000000))
         p = Payment(reference_number=ref,
-                    transaction_id=str(uuid4()),
+                    transaction_id=str(uuid4())[0:29],
                     payment_day=datetime.now(),
                     amount=Decimal(amount),
                     type="XYZ",
                     payer_name=membership.name())
         p.save()
+        transaction.commit()
         return p
 
 
@@ -124,6 +130,7 @@ def create_payment(membership):
 def main():
     if Membership.objects.count() > 0 or Payment.objects.count() > 0:
         print "Database not empty, refusing to generate test data"
+        transaction.commit()
         sys.exit(1)
     # Approved members
     for i in xrange(1,1000):
