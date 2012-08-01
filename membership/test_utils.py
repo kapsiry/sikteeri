51e98f6983c6201fcdc4cb2c58d9676d4f7b7324
Antti Jaakkola <annttu@kapsi.fi>
2012-08-01 10:27:46 +0300

    rebased + pep8-compatible

--- membership/test_utils.py
+++ membership/test_utils.py
@@ -6,50 +6,56 @@ import logging
 first_names = [
     u"Iines", u"Aku", u"Minni", u"Mikki", u"Tupu", u"Hupu", u"Lupu",
     u"Leenu", u"Liinu", u"Tiinu", u"Pluto", u"Roope", u"Taavi", u"Touho",
-    u"Kulta-Into", u"Kroisos", u"Velmu", u"Pelle", u"Hannu", u"Hansu", u"Mummo",
-    u"Milla", u"Toope", u"Mortti", u"Vertti", u"Hessu", u"Polle", u"Heluna",
-    u"Klaara", u"Sisu", u"Matami", u"Tiku", u"Tiku", u"Veli", u"Sepe",
-    u"Tipsu", u"Timotei", u"Bumba", u"Dumbo", u"Jopi", u"Neiti"]
+    u"Kulta-Into", u"Kroisos", u"Velmu", u"Pelle", u"Hannu", u"Hansu",
+    u"Mummo", u"Milla", u"Toope", u"Mortti", u"Vertti", u"Hessu", u"Polle",
+    u"Heluna", u"Klaara", u"Sisu", u"Matami", u"Tiku", u"Tiku", u"Veli",
+    u"Sepe", u"Tipsu", u"Timotei", u"Bumba", u"Dumbo", u"Jopi", u"Neiti"]
 
 # Some last names taken from Disney cartoons
 last_names = [
     u"Ankka", u"Hiiri", u"Koninkaulus", u"Peloton", u"Hanhi", u"Pennonen",
     u"Pii", u"Magia", u"Hopo", u"Kotro", u"Jalkapuoli", u"Kaasi", u"Kani",
-    u"Susi", u"Hukka",u"Ponteva", u"Sirkka", u"Näpsä", u"Musta"
-    ]
+    u"Susi", u"Hukka", u"Ponteva", u"Sirkka", u"Näpsä", u"Musta"]
 
 # Random finctonal municipalities
 municipalities = [
-    u"Ankkalinna", u"Hanhivaara", u"Mummola", u"Duckburg", u"Kaukastan", u"Takametsä",
-    u"Timpuktu", u"Toisaala", u"Mäentaus", u"Vuorisola", u"Mikälie", u"Jokimutka"
-    ]
+    u"Ankkalinna", u"Hanhivaara", u"Mummola", u"Duckburg", u"Kaukastan",
+    u"Takametsä", u"Timpuktu", u"Toisaala", u"Mäentaus", u"Vuorisola",
+    u"Mikälie", u"Jokimutka"]
 
 # Fictional streets
 street_addresses = [
-    u"Paratiisitie", u"Onnitie", "Pääkatu", "Sivukuja", u"Jullentie", u"Säiliökatu",
-    u"Rahakuja", u"Rosvotie", u"Hassukuja", u"Puutarhakatu", u"Lähitie"
-    ]
+    u"Paratiisitie", u"Onnitie", "Pääkatu", "Sivukuja", u"Jullentie",
+    u"Säiliökatu", u"Rahakuja", u"Rosvotie", u"Hassukuja", u"Puutarhakatu",
+    u"Lähitie"]
+
 
 def random_first_name():
     return choice(first_names)
 
+
 def random_last_name():
     return choice(last_names)
+
 
 def random_municipality():
     return choice(municipalities)
 
+
 def random_street():
     return choice(street_addresses)
+
 
 def random_email(firstname, lastname):
     firstname = firstname.lower()
     lastname = lastname.lower()
     return ("%s.%s@example.com" % (firstname, lastname))
 
+
 class MockLoggingHandler(logging.Handler):
     """Mock logging handler to check for expected logs as per:
-    <http://stackoverflow.com/questions/899067/how-should-i-verify-a-log-message-when-testing-python-code-under-nose/1049375#1049375>"""
+    <http://stackoverflow.com/questions/899067/how-should-i-verify-a-log-message-when-testing-python-code-under-nose/1049375#1049375>
+    """
     def __init__(self, *args, **kwargs):
         self.reset()
         logging.Handler.__init__(self, *args, **kwargs)
--- sikteeri/generate_test_data.py
+++ sikteeri/generate_test_data.py
@@ -12,7 +12,8 @@ from random import random, randint, choice
 from uuid import uuid4
 from decimal import Decimal
 import logging
-from membership.test_utils import random_first_name, random_last_name, random_municipality, random_email, random_street
+from membership.test_utils import random_first_name, random_municipality,
+from membership.test_utils import random_last_name, random_email, random_street
 logger = logging.getLogger("generate_test_data")
 
 from datetime import datetime
@@ -34,26 +35,27 @@ from services.models import Alias, Service, ServiceType
 
 if Fee.objects.all().count() == 0:
     sys.exit("No fees in the database. Did you load fixtures into the " +
-	     "database first?\n (./manage.py loaddata test_data.json)")
+             "database first?\n (./manage.py loaddata test_data.json)")
 
 user = User.objects.get(id=1)
+
 
 @transaction.commit_on_success
 def create_dummy_member(i):
     fname = random_first_name()
     lname = random_last_name()
     d = {
-        'street_address' : '%s %d'%(random_street(), i),
-        'postal_code' : '%d' % (i+1000),
-        'post_office' : '%s' % random_municipality(),
-        'country' : 'Finland',
-        'phone' : "%09d" % (40123000 + i),
-        'sms' : "%09d" % (40123000 + i),
-        'email' : '%s' % random_email(fname, lname),
-        'homepage' : 'http://www.example.com/%d'%i,
-        'first_name' : fname,
-        'given_names' : '%s %s' % (fname, u"Testikäyttäjä"),
-        'last_name' : lname,
+        'street_address': '%s %d' % (random_street(), i),
+        'postal_code': '%d' % (i + 1000),
+        'post_office': '%s' % random_municipality(),
+        'country': 'Finland',
+        'phone': "%09d" % (40123000 + i),
+        'sms': "%09d" % (40123000 + i),
+        'email': '%s' % random_email(fname, lname),
+        'homepage': 'http://www.example.com/%d' % i,
+        'first_name': fname,
+        'given_names': '%s %s' % (fname, u"Testikäyttäjä"),
+        'last_name': lname,
     }
 
     if duplicate_of is not None:
@@ -85,26 +87,35 @@ def create_dummy_member(i):
     login_alias.save()
 
     # Services
-    forward_alias_service = Service(servicetype=ServiceType.objects.get(servicetype='Email alias'),
-                                    alias=forward_alias, owner=membership, data=forward_alias.name)
+    forward_alias_service = Service(servicetype=ServiceType.objects.get(
+                                    servicetype='Email alias'),
+                                    alias=forward_alias, owner=membership,
+                                    data=forward_alias.name)
     forward_alias_service.save()
 
-    unix_account_service = Service(servicetype=ServiceType.objects.get(servicetype='UNIX account'),
-                                   alias=login_alias, owner=membership, data=login_alias.name)
+    unix_account_service = Service(servicetype=ServiceType.objects.get(
+                                   servicetype='UNIX account'),
+                                   alias=login_alias, owner=membership,
+                                   data=login_alias.name)
     unix_account_service.save()
 
     if random() < 0.6:
-        mysql_service = Service(servicetype=ServiceType.objects.get(servicetype='MySQL database'),
-                                alias=login_alias, owner=membership, data=login_alias.name.replace('-', '_'))
+        mysql_service = Service(servicetype=ServiceType.objects.get(
+                                servicetype='MySQL database'),
+                                alias=login_alias, owner=membership,
+                                data=login_alias.name.replace('-', '_'))
         mysql_service.save()
     if random() < 0.6:
-        postgresql_service = Service(servicetype=ServiceType.objects.get(servicetype='PostgreSQL database'),
-                                     alias=login_alias, owner=membership, data=login_alias.name)
+        postgresql_service = Service(servicetype=ServiceType.objects.get(
+                                     servicetype='PostgreSQL database'),
+                                     alias=login_alias, owner=membership,
+                                     data=login_alias.name)
         postgresql_service.save()
     # End of services
 
     logger.info("New application %s from %s:." % (str(person), '::1'))
     return membership
+
 
 @transaction.commit_on_success
 def create_payment(membership):
@@ -112,8 +123,9 @@ def create_payment(membership):
         amount = "35.0"
         if random() < 0.2:
             amount = "30.0"
-            
-        ref = generate_membership_bill_reference_number(membership.id, datetime.now().year)
+
+        ref = generate_membership_bill_reference_number(membership.id,
+                                                        datetime.now().year)
         if random() < 0.2:
             ref = str(randint(1000, 1000000))
         p = Payment(reference_number=ref,
@@ -125,13 +137,14 @@ def create_payment(membership):
         p.save()
         return p
 
+
 @transaction.commit_on_success
 def main():
     if Membership.objects.count() > 0 or Payment.objects.count() > 0:
         print "Database not empty, refusing to generate test data"
         sys.exit(1)
     # Approved members
-    for i in xrange(1,1000):
+    for i in xrange(1, 1000):
         membership = create_dummy_member(i)
         if membership:
             membership.preapprove(user)
@@ -139,18 +152,18 @@ def main():
             create_payment(membership)
 
     # Pre-approved members
-    for i in xrange(1000,1100):
+    for i in xrange(1000, 1100):
         membership = create_dummy_member(i)
         if membership:
             membership.preapprove(user)
 
     # New applications
-    for i in xrange(1100,1190):
+    for i in xrange(1100, 1190):
         membership = create_dummy_member(i)
 
     # Make a few duplicates for duplicate detection GUI testing
-    for i in xrange(1190,1200):
-        duplicate_of = Membership.objects.get(id=i-10)
+    for i in xrange(1190, 1200):
+        duplicate_of = Membership.objects.get(id=i - 10)
         membership = create_dummy_member(i + 10, duplicate_of=duplicate_of)
         transaction.commit()
 
