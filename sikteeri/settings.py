5dd8461b55fc873e5a7a1153cda81bc14eebe977
Joonas Kuorilehto <joneskoo@derbian.fi>
2014-07-04 21:34:56 +0300

    Add hygiene to settings files

--- sikteeri/settings.py
+++ sikteeri/settings.py
@@ -1,21 +1,44 @@
-# Django settings for sikteeri project.
+# Django development settings for sikteeri project.
+# For production use, override with
+#
+#   --settings=production_settings
+#
+# and have production_settings.py
+#
+#   import * from settings
+#
 
 import django.conf.global_settings as DEFAULT_SETTINGS
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+DEBUG = True
+TEMPLATE_DEBUG = True
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    # ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
 
+# brew install postgresql && createdb sikteeri
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sikteeri',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'Europe/Helsinki'
 SHORT_DATE_FORMAT = 'd.m.Y'
 
@@ -29,13 +52,30 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# For development purposes, relative or absolute path to static assets
-STATIC_ROOT = 'sikteeri/static/'
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
 
-# For development purposes
-MEMBERSHIP_STATIC_ROOT = 'membership/static/'
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
 
-# This is the path used in templates for asset URL
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
@@ -43,6 +83,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'sikteeri/static',
+    'membership/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -53,7 +95,8 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-LOGIN_URL = '/login/'
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -67,14 +110,16 @@ TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'sikteeri.ForceDefaultLanguageMiddleware.ForceDefaultLanguageMiddleware',
     'sikteeri.GitVersionHeaderMiddleware.GitVersionHeaderMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'sikteeri.urls'
@@ -94,44 +139,87 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.humanize',
     'django.contrib.comments',
+    'django.contrib.staticfiles',
     'membership',
     'services',
     'south',
 )
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 
 SOUTH_TESTS_MIGRATE = False
 
 # If set, this string will be displayed and sikteeri is disabled
 MAINTENANCE_MESSAGE = None
 
-SESSION_COOKIE_SECURE = True
-PRODUCTION = True
-
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
-USE_X_FORWARDED_HOST = True
+#USE_X_FORWARDED_HOST = True
 
 # Show 30 items per page in listview
 ENTRIES_PER_PAGE=30
 
-from local_settings import *
+# Secure cookies require https
+# SESSION_COOKIE_SECURE = False
 
-# Sanity checks for settings
-from os.path import exists as path_exists
+# Hosts allowed to fetch statistics etc. without authentication
+TRUSTED_HOSTS = ['127.0.0.1', '::1']
 
-assert(IBAN_ACCOUNT_NUMBER != None)
-assert(BIC_CODE != None)
-assert(BILLING_FROM_EMAIL != None)
-BILLING_CC_EMAIL
-assert(FROM_EMAIL != None)
-assert(SYSADMIN_EMAIL != None)
-assert(SECRET_KEY != None)
-assert(BILL_SUBJECT != None)
-assert(ENABLE_REMINDERS != None)
-assert(REMINDER_GRACE_DAYS != None)
-assert(BILL_DAYS_BEFORE_CYCLE != None)
-assert(BILL_DAYS_TO_DUE != None)
-assert(TRUSTED_HOSTS != None)
+# Billing settings
+IBAN_ACCOUNT_NUMBER = 'FI00 0000 0000 0000 00'
+BIC_CODE = 'AAAAAAAA'
+BILLING_FROM_EMAIL = 'from@example.com'
+BILLING_CC_EMAIL = None
+BILL_SUBJECT = 'Test bill, ignore'
+REMINDER_SUBJECT = 'Test reminder, ignore'
+# Send bills 2 weeks before new cycle starts
+BILL_DAYS_BEFORE_CYCLE = 14 # days
+# New bills will have a due date 2 weeks in the future
+BILL_DAYS_TO_DUE = 14 # days
+# Send reminders 2 weeks after previous due day
+REMINDER_GRACE_DAYS = 14 # days
+ENABLE_REMINDERS = False
+# Reminders will be additionally sent to account@domain if specified
+UNIX_EMAIL_DOMAIN = 'example.com'
 
-assert 'PAPER_REMINDER_TEMPLATE' in locals(), "Paper reminder template should be defined"
-assert path_exists(PAPER_REMINDER_TEMPLATE), "Paper reminder LaTeX template file should exist"
+# Generic email settings
+FROM_EMAIL = 'from.nonbill@example.com'
+SYSADMIN_EMAIL = 'admins@example.com'
+
+# Don't really send email messages, only show them on console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_SUBJECT_PREFIX = '[sikteeri-TEST] '
+
+# Paper reminder templates
+PAPER_REMINDER_TEMPLATE = '/dev/null'
+
+# When PRODUCTION is true, show final graphics and colours.
+# Otherwise indicate that this is a development environment (logo, colour)
+PRODUCTION = False
--- sikteeri/urls.py
+++ sikteeri/urls.py
@@ -1,31 +1,26 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^sikteeri/', include('sikteeri.foo.urls')),
+    # Examples:
+    # url(r'^$', 'sikteeri.views.home', name='home'),
+    # url(r'^sikteeri/', include('sikteeri.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^$', 'sikteeri.views.frontpage', name='frontpage'),
     url(r'^comments/', include('django.contrib.comments.urls')),
-
-    url(r'^static/membership/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEMBERSHIP_STATIC_ROOT}),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^membership/', include('membership.urls')),
     url(r'^services/', include('services.urls')),
 
     url(r'^login/', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'},
+        name='logout'),
 
-    url(r'^', 'sikteeri.views.frontpage', name='frontpage'),
-    # url(r'^', 'django.views.generic.simple.direct_to_template', {'template': 'frontpage.html'}, name='frontpage'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 )
