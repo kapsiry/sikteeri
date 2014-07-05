fdde80d321b272be9bbfcc65e99bd67dc1650262
Joonas Kuorilehto <joneskoo@derbian.fi>
2014-07-05 12:46:11 +0300

    Further clean up development settings

--- sikteeri/settings.py
+++ sikteeri/settings.py
@@ -8,42 +8,37 @@
 #   import * from settings
 #
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 import django.conf.global_settings as DEFAULT_SETTINGS
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'z$g77fzdhwi%lrn-8zezn995*=m!s_nb*k&7=fo(9ppf6(s%_a'
 
-MANAGERS = ADMINS
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
 
-# brew install postgresql && createdb sikteeri
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sikteeri',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
-
-# Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
+# http://www.i18nguy.com/unicode/language-identifiers.html
 TIME_ZONE = 'Europe/Helsinki'
 SHORT_DATE_FORMAT = 'd.m.Y'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'fi-fi'
 
 SITE_ID = 1
@@ -59,84 +54,41 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    'sikteeri/static',
-    'membership/static',
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'z$g77fzdhwi%lrn-8zezn995*=m!s_nb*k&7=fo(9ppf6(s%_a'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+)
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'sikteeri/static'),
+    os.path.join(BASE_DIR, 'membership/static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'sikteeri/templates/'),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
     "sikteeri.context_processors.is_production",
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'sikteeri.ForceDefaultLanguageMiddleware.ForceDefaultLanguageMiddleware',
-    'sikteeri.GitVersionHeaderMiddleware.GitVersionHeaderMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'sikteeri.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "sikteeri/templates/",
-)
-
 INSTALLED_APPS = (
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.admin',
+    'django.contrib.messages',
     'django.contrib.humanize',
     'django.contrib.comments',
     'django.contrib.staticfiles',
@@ -145,6 +97,25 @@ INSTALLED_APPS = (
     'south',
     'debug_toolbar',
 )
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'sikteeri.ForceDefaultLanguageMiddleware.ForceDefaultLanguageMiddleware',
+    'sikteeri.GitVersionHeaderMiddleware.GitVersionHeaderMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'sikteeri.urls'
+
+WSGI_APPLICATION = 'sikteeri.wsgi.application'
+
+LOGIN_URL = 'login'
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -175,8 +146,6 @@ LOGGING = {
     }
 }
 
-LOGIN_URL = 'login'
-
 SOUTH_TESTS_MIGRATE = False
 
 # If set, this string will be displayed and sikteeri is disabled
@@ -184,13 +153,8 @@ MAINTENANCE_MESSAGE = None
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
-#USE_X_FORWARDED_HOST = True
-
 # Show 30 items per page in listview
 ENTRIES_PER_PAGE=30
-
-# Secure cookies require https
-# SESSION_COOKIE_SECURE = False
 
 # Hosts allowed to fetch statistics etc. without authentication
 TRUSTED_HOSTS = ['127.0.0.1', '::1']
@@ -223,6 +187,6 @@ EMAIL_SUBJECT_PREFIX = '[sikteeri-TEST] '
 # Paper reminder templates
 PAPER_REMINDER_TEMPLATE = '/dev/null'
 
-# When PRODUCTION is true, show final graphics and colours.
+# When PRODUCTION is true, show production graphics and colours.
 # Otherwise indicate that this is a development environment (logo, colour)
 PRODUCTION = False
