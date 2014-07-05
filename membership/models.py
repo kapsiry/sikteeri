318c6110055e411e7acd39504acc0ac11e090647
Joonas Kuorilehto <joneskoo@derbian.fi>
2014-07-05 11:50:25 +0300

    Set public_membership default to False explicitly

--- membership/models.py
+++ membership/models.py
@@ -182,7 +182,7 @@ class Membership(models.Model):
     created = models.DateTimeField(auto_now_add=True, verbose_name=_('Membership created'))
     approved = models.DateTimeField(blank=True, null=True, verbose_name=_('Membership approved'))
     last_changed = models.DateTimeField(auto_now=True, verbose_name=_('Membership changed'))
-    public_memberlist = models.BooleanField(_('Show in the memberlist'))
+    public_memberlist = models.BooleanField(_('Show in the memberlist'), default=False)
 
     municipality = models.CharField(_('Home municipality'), max_length=128, blank=True)
     nationality = models.CharField(_('Nationality'), max_length=128)
