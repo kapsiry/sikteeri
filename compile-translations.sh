54a707ae3e7751276e12f048c00ae6c9b143e782
Joonas Kuorilehto <joneskoo@derbian.fi>
2015-05-14 18:05:41 +0300

    Add parameter for new strings in translations

--- compile-translations.sh
+++ compile-translations.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+NEW=$1
+
+# If new strings have been introduced, translations need
+# to be updated.
+#
+# Run:
+#
+# ./compile-translations.sh new
+
 for i in membership sikteeri services; do
-	(cd $i && django-admin.py compilemessages)
+	(
+		cd $i
+		if [ "$1" = "new" ]; then
+			django-admin.py makemessages -a
+			django-admin.py makemessages -a -e js
+		fi
+		django-admin.py compilemessages
+	)
 done
