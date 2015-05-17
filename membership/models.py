7cca6b77b2f5959e9152cf3d20749e4aa9725949
Joonas Kuorilehto <joneskoo@derbian.fi>
2015-05-17 11:52:15 +0300

    Change membership charfields to not null
    If charfield can be null, there will be two empty values
    for the field: None and ''. This was the reason why
    saving membership changed organization_registration_number.

--- membership/models.py
+++ membership/models.py
@@ -196,7 +196,8 @@ class Membership(models.Model):
     municipality = models.CharField(_('Home municipality'), max_length=128, blank=True)
     nationality = models.CharField(_('Nationality'), max_length=128)
     birth_year = models.IntegerField(_('Year of birth'), null=True, blank=True)
-    organization_registration_number = models.CharField(_('Organization registration number'), max_length=15, null=True, blank=True)
+    organization_registration_number = models.CharField(_('Organization registration number'),
+                                                        blank=True, max_length=15)
 
     person = models.ForeignKey('Contact', related_name='person_set', verbose_name=_('Person'), blank=True, null=True)
     billing_contact = models.ForeignKey('Contact', related_name='billing_set', verbose_name=_('Billing contact'), blank=True, null=True)
@@ -381,7 +382,7 @@ class Membership(models.Model):
         self.organization = None
         self.municipality = ''
         self.birth_year = None
-        self.organization_registration_number = None
+        self.organization_registration_number = ''
         self.save()
         for contact in contacts:
             if contact != None:
@@ -867,7 +868,7 @@ class Payment(models.Model):
     # extension
     billingcycle = models.ForeignKey('BillingCycle', verbose_name=_('Cycle'), null=True)
     ignore = models.BooleanField(default=False, verbose_name=_('Ignored payment'))
-    comment = models.CharField(max_length=64, verbose_name=_('Comment'), null=True)
+    comment = models.CharField(max_length=64, verbose_name=_('Comment'), blank=True)
 
     reference_number = models.CharField(max_length=64, verbose_name=_('Reference number'), blank=True)
     message = models.CharField(max_length=256, verbose_name=_('Message'), blank=True)
@@ -936,8 +937,7 @@ class ApplicationPoll(models.Model):
 
     membership = models.ForeignKey('Membership', verbose_name=_('Membership'))
     date = models.DateTimeField(auto_now=True, verbose_name=_('Timestamp'))
-    answer = models.CharField(max_length=512,verbose_name=_('Service specific data'),
-                                  blank=False, null=False)
+    answer = models.CharField(max_length=512, verbose_name=_('Service specific data'))
 
 models.signals.post_save.connect(logging_log_change, sender=Membership)
 models.signals.post_save.connect(logging_log_change, sender=Contact)
