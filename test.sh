a78dbb64c856a87f1f6b667450828be104ce11de
Joonas Kuorilehto <joneskoo@derbian.fi>
2015-11-01 11:22:34 +0200

    Restore coverage of migrations and tests, remove coveragerc

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 export DJANGO_SETTINGS_MODULE=sikteeri.settings
 export SIKTEERI_CONFIG=dev
 
-py.test --cov-config .coveragerc --cov=membership --cov=sikteeri --cov=services
+py.test --cov=membership --cov=sikteeri --cov=services
