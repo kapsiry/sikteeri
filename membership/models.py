08f645283796ca81178473426a0b37eac0e3ba83
Joonas Kuorilehto <joneskoo@derbian.fi>
2015-11-06 21:50:44 +0200

    Remove duplicate “billing_contact” from dict

--- membership/models.py
+++ membership/models.py
@@ -828,7 +828,6 @@ class Bill(models.Model):
                 'street_address': membership.get_billing_contact().street_address,
                 'postal_code': membership.get_billing_contact().postal_code,
                 'post_office': membership.get_billing_contact().post_office,
-                'billing_contact': membership.billing_contact,
                 'billingcycle': self.billingcycle,
                 'iban_account_number': settings.IBAN_ACCOUNT_NUMBER,
                 'bic_code': settings.BIC_CODE,
