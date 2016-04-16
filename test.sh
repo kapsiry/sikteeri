932ed61a7552dbab0bc3552e7431c3810d419c10
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-04-16 16:02:43 +0300

    Generate junit output in tests

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 export DJANGO_SETTINGS_MODULE=sikteeri.settings
 export SIKTEERI_CONFIGURATION=dev
 
-py.test --cov=membership --cov=sikteeri --cov=services
+py.test --cov=membership --cov=sikteeri --cov=services --junit-xml=junit.xml
