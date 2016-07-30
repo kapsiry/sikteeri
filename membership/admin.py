c2211171273f8d6b2796efe4f33e9d24d1e806b9
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-07-30 18:08:14 +0300

    Django admin: Fix huge dropdowns
    Fixed huge dropdown selection in
     - alias edit page for membership selection
     - membership edit page contact selection
    
    Also made Contacts searchable in Django Admin.

--- membership/admin.py
+++ membership/admin.py
@@ -2,8 +2,30 @@ from django.contrib import admin
 from membership.models import Membership, Contact, Fee, BillingCycle, Bill,\
     Payment
 
-admin.site.register(Membership)
-admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    search_fields = [
+        'first_name',
+        'given_names',
+        'last_name',
+        'organization_name',
+        'email',
+    ]
+
+
+class MembershipAdmin(admin.ModelAdmin):
+    # search_fields has to be set so get_search_results is called
+    search_fields = ['*ignored*']
+    # Display as text id field, with search button
+    raw_id_fields = ['person', 'billing_contact', 'tech_contact', 'organization']
+
+    def get_search_results(self, request, queryset, search_term):
+        """Use same search as on Sikteeri site for Django Admin"""
+        qs = Membership.search(search_term)
+        return qs, False
+
+admin.site.register(Membership, MembershipAdmin)
+admin.site.register(Contact, ContactAdmin)
 admin.site.register(Fee)
 admin.site.register(BillingCycle)
 admin.site.register(Bill)
